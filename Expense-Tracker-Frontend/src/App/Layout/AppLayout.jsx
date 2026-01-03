a0313f18c3fa7edfc4adbc81baaea6463cde3fbb
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-04 05:01:09 +0530

    add form to app layout

--- Expense-Tracker-Frontend/src/App/Layout/AppLayout.jsx
+++ Expense-Tracker-Frontend/src/App/Layout/AppLayout.jsx
@@ -1,4 +1,5 @@
 import AmountCard from '../components/AmountCard'
+import InputForm from '../components/InputForm'
 import TopBar from '../components/TopBar'
 
 function AppLayout() {
@@ -6,6 +7,12 @@ function AppLayout() {
     <div className="px-3 sm:px-6 md:px-10 lg:px-30 bg-gray-950 h-screen text-white">
       <TopBar />
       <AmountCard />
+      <div className="py-2">
+        <h3 className="text-2xl tracking-wider font-semibold pb-3 border-b border-white/30">
+          Enter Expense
+        </h3>
+        <InputForm />
+      </div>
     </div>
   )
 }
