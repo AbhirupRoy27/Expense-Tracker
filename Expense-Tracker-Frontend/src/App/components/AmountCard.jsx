ac1b934badaeb836f2e1ce9fde88bc7d06d92696
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-07 01:16:28 +0530

    feat: display current month in AmountCard header and improve CategorySelector layout

--- Expense-Tracker-Frontend/src/App/components/AmountCard.jsx
+++ Expense-Tracker-Frontend/src/App/components/AmountCard.jsx
@@ -3,12 +3,29 @@ import { useSelector } from 'react-redux'
 function AmountCard() {
   const { amount } = useSelector((s) => s.input)
 
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+
+  const month = months[new Date().getMonth()]
+
   return (
     <div className="w-full sm:w-auto  bg-white/30 my-6 rounded-2xl">
       <div className="min-h-15 w-full bg-white/50 rounded-tr-2xl rounded-tl-2xl border-b-3 border-white object-cover"></div>
       <div className="px-4 pt-10 pb-5">
         <h2 className="text-2xl mb-2 font-extralight tracking-wider border-b border-white/30 w-fit pb-2">
-          Total Spend
+          Total Spend ({month})
         </h2>
         <h1 className="text-4xl text-right font-bold">Rs. {amount}</h1>
       </div>
--- Expense-Tracker-Frontend/src/App/components/CategorySelector.jsx
+++ Expense-Tracker-Frontend/src/App/components/CategorySelector.jsx
@@ -15,17 +15,20 @@ function CategorySelector({ value, setInputs }) {
   }
 
   return (
-    <select
-      className="py-4 px-2 my-1 bg-white/10 focus:bg-white/20 focus:outline-0"
-      onChange={handleChange}
-      value={value}
-      name="category"
-    >
-      {options.map((opt) => (
-        <option key={opt.id}>{opt.option}</option>
-      ))}
-      <option defaultValue={'Others'}>Others</option>
-    </select>
+    <>
+      <label>Select Category</label>
+      <select
+        className="py-4 px-2 bg-white/10 focus:bg-white/20 focus:outline-0"
+        onChange={handleChange}
+        value={value}
+        name="category"
+      >
+        {options.map((opt) => (
+          <option key={opt.id}>{opt.option}</option>
+        ))}
+        <option defaultValue={'Others'}>Others</option>
+      </select>
+    </>
   )
 }
 
