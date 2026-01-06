3c08952aaf6dc1db67716e9e9d61b8e3f4ee5147
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-07 00:29:50 +0530

    fix: update App component structure for consistent layout

--- Expense-Tracker-Frontend/src/App/App.jsx
+++ Expense-Tracker-Frontend/src/App/App.jsx
@@ -3,11 +3,11 @@ import PublicRoute from './Routes/PublicRoute'
 
 function App() {
   return (
-    <>
+    <div className=" px-3 sm:px-6 md:px-10 lg:px-30 bg-gray-950 h-screen text-white">
       <Routes>
         <Route path="/*" element={<PublicRoute />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
