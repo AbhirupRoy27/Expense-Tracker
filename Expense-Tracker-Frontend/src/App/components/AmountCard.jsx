31dd44533b16760bd769988a4a9e60053cdd8029
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-08 03:16:31 +0530

    feat: enhance AmountCard layout with month display and improved styling

--- Expense-Tracker-Frontend/src/App/components/AmountCard.jsx
+++ Expense-Tracker-Frontend/src/App/components/AmountCard.jsx
@@ -1,3 +1,4 @@
+import { Calendar } from 'lucide-react'
 import { useSelector } from 'react-redux'
 
 function AmountCard() {
@@ -22,12 +23,16 @@ function AmountCard() {
 
   return (
     <div className="w-full sm:w-auto  bg-white/30 my-6 rounded-2xl">
-      <div className="min-h-15 w-full bg-white/50 rounded-tr-2xl rounded-tl-2xl border-b-3 border-white object-cover"></div>
-      <div className="px-4 pt-10 pb-5">
+      <div className="min-h-15 w-full bg-white/50 rounded-tr-2xl rounded-tl-2xl border-b-3 border-white object-cover flex items-center">
+        <h1 className="text-3xl font-semibold tracking-wider text-gray-950 px-4 flex gap-2 items-center">
+          {month} <Calendar size={30} />
+        </h1>
+      </div>
+      <div className="px-1 py-5">
         <h2 className="text-2xl mb-2 font-extralight tracking-wider border-b border-white/30 w-fit pb-2">
-          Total Spend ({month})
+          Total Spend
         </h2>
-        <h1 className="text-4xl text-right font-bold">Rs. {amount}</h1>
+        <h1 className="text-4xl text-right font-bold px-2">Rs. {amount}</h1>
       </div>
     </div>
   )
