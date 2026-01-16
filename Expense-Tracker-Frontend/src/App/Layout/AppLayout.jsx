20eedef6d43f1648139730dfed5a62369d5d0ae2
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-16 07:38:48 +0530

    refactor: simplify AppLayout by removing unused components and improving layout structure

--- Expense-Tracker-Frontend/src/App/Layout/AppLayout.jsx
+++ Expense-Tracker-Frontend/src/App/Layout/AppLayout.jsx
@@ -1,44 +1,17 @@
-import { useNavigate } from 'react-router-dom'
-import AmountCard from '../components/AmountCard'
+import { Outlet } from 'react-router-dom'
 import InputForm from '../components/InputForm'
 import TopBar from '../components/TopBar'
-import { Wallet } from 'lucide-react'
+
 // import { useDispatch } from 'react-redux'
 // import { resetAmount } from '../../redux/features/inputSlice'
 
 function AppLayout() {
-  const navigate = useNavigate()
-
   // const dispatch = useDispatch()
 
   return (
-    <div className=" bg-gray-950 h-screen text-white">
+    <div className="bg-gray-950 h-screen text-white min-w-95">
       <TopBar />
-      <AmountCard />
-      <div className="py-2">
-        <h3 className="text-2xl tracking-wider font-semibold pb-3 border-b border-white/30">
-          Enter Expense
-        </h3>
-        <InputForm />
-      </div>
-      <div className="flex gap-2">
-        <button
-          className="bg-emerald-600 text-black py-3 px-4 flex gap-2 font-semibold"
-          onClick={() => navigate('/spend')}
-        >
-          <Wallet /> See All Spends
-        </button>
-        <button
-          className="bg-emerald-600 text-black py-3 px-4 flex gap-2 font-semibold"
-          onClick={() => {
-            alert('not functionable')
-            // localStorage.clear('total')
-            // dispatch(resetAmount())
-          }}
-        >
-          <Wallet /> Clear
-        </button>
-      </div>
+      <Outlet />
     </div>
   )
 }
