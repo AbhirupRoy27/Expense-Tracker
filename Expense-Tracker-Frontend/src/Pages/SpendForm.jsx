1d91a9e307fc7774c532c56b46255caaa481f16b
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-16 07:39:03 +0530

    feat: add SpendForm component for expense entry and navigation

---
+++ Expense-Tracker-Frontend/src/Pages/SpendForm.jsx
@@ -0,0 +1,39 @@
+import AmountCard from '../App/components/AmountCard'
+import { useNavigate } from 'react-router-dom'
+import InputForm from '../App/components/InputForm'
+import { Wallet } from 'lucide-react'
+
+function SpendForm() {
+  const navigate = useNavigate()
+  return (
+    <div className="min-w-95">
+      <AmountCard />
+      <div className="py-2">
+        <h3 className="text-2xl tracking-wider font-semibold pb-3 border-b border-white/30">
+          Enter Expense
+        </h3>
+        <InputForm />
+      </div>
+      <div className="flex gap-2">
+        <button
+          className="bg-emerald-600 text-black py-3 px-4 flex gap-2 font-semibold"
+          onClick={() => navigate('/spend')}
+        >
+          <Wallet /> See All Spends
+        </button>
+        <button
+          className="bg-emerald-600 text-black py-3 px-4 flex gap-2 font-semibold"
+          onClick={() => {
+            alert('not functionable')
+            // localStorage.clear('total')
+            // dispatch(resetAmount())
+          }}
+        >
+          <Wallet /> Clear
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SpendForm
