e0ce6c04c024db56c48b2f9ebcd1111cc76aee34
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-01-16 07:38:54 +0530

    refactor: restructure PublicRoute to improve routing organization and clarity

--- Expense-Tracker-Frontend/src/App/Routes/PublicRoute.jsx
+++ Expense-Tracker-Frontend/src/App/Routes/PublicRoute.jsx
@@ -2,19 +2,22 @@ import { Route, Routes } from 'react-router-dom'
 import AppLayout from '../Layout/AppLayout'
 import SpendsPage from '../../Pages/SpendsPage'
 import TranasactionContextProvider from '../../context/TranasactionContext'
+import SpendForm from '../../Pages/SpendForm'
 
 function PublicRoute() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />} />
-      <Route
-        path="/spend"
-        element={
-          <TranasactionContextProvider>
-            <SpendsPage />
-          </TranasactionContextProvider>
-        }
-      />
+      <Route path="/*" element={<AppLayout />}>
+        <Route index element={<SpendForm />} />
+        <Route
+          path="spend"
+          element={
+            <TranasactionContextProvider>
+              <SpendsPage />
+            </TranasactionContextProvider>
+          }
+        />
+      </Route>
     </Routes>
   )
 }
